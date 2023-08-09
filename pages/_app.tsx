386fccd3d452df331a5bdfdae8f973e89fddb6f4
myreceiptt <nota@straight-line.org>
2023-08-09 20:08:10 +0700

    clientID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,19 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Polygon;
+// const activeChainId = ChainId.Polygon;
+const activeChain = "mumbai";
+const activeClientId = "507db7365d0ff5a6d7057de30203aa7f";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <Component {...pageProps} />
+    // <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      // clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      clientId={activeClientId}
+    >
+      <Component {...pageProps} /> 
     </ThirdwebProvider>
   );
 }
